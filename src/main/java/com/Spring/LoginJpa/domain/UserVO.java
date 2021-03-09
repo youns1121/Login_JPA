6f319387fe5c0062635dc5e29516f366d2f8fd41
youns <minys94@daum.net>
2021-03-10 02:37:04 +0900

    2021-03-10 Spring Security 구현시작

--- src/main/java/com/Spring/LoginJpa/domain/UserVO.java
+++ src/main/java/com/Spring/LoginJpa/domain/UserVO.java
@@ -2,11 +2,13 @@ package com.Spring.LoginJpa.domain;
 
 
 import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.time.LocalDateTime;
 
 
 @Getter
@@ -18,10 +20,14 @@ public class UserVO {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String userId;
+
+    private String userid;
     private String password;
-    private String userName;
+    private String username;
     private String role;
+    // 계정 생성일
+    @CreationTimestamp
+    private LocalDateTime createdDate;
 
 
 }
